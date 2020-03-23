392015ef98545efecf7921c3a2ccb67279441eb3
David Hodgkinson <david@davidhodgkinson.net>
2020-03-23 21:52:21 +0000

    Support for Windows?

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,42 @@
 
 #include <iostream>
 
-int main()
+void run()
 {
     containers::Vector<int>* v = new containers::Vector<int>();
-    for (int i = 0 ; i < 5000 ; ++i)
+    for (int i = 0; i < 5000; ++i)
     {
         v->push_back(i);
     }
 
-    for (int i = 0 ; i < v->size() ; ++i)
+    for (int i = 0; i < v->size(); ++i)
     {
         std::cout << (*v)[i] << std::endl;
     }
-    
+
     delete v;
+}
+
+#if defined(WIN32)
+
+#include "Windows.h"
+
+INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+{
+    run();
 
     return 0;
 }
+
+#else
+
+int main()
+{
+    run();
+
+    return 0;
+}
+
+#endif
+
+
